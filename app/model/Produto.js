5b61528ff37f26e600412af90739119eb37ad0a2
Brian_Izaki <bkizaki@outlook.com>
2020-11-13 11:21:51 -0300

    refactor: alterado a função de exportação da model

--- app/model/Produto.js
+++ app/model/Produto.js
@@ -1,31 +1,29 @@
 const mongoose = require('mongoose');
 
-module.exports = function () {
-  const schema = mongoose.Schema({
-    'descricao': {
-      type: String,
-      required: true,
-    },
-    'valor': {
-      type: Number,
-      required: true,
-      min: [0, 'Valor deve ser sempre positivo']
+const schema = mongoose.Schema({
+  'descricao': {
+    type: String,
+    required: true,
+  },
+  'valor': {
+    type: Number,
+    required: true,
+    min: [0, 'Valor deve ser sempre positivo']
 
-    },
-    'created': {
-      type: Date,
-      default: Date.now(),
-    },
-    'dbref': {
-      type: mongoose.Schema.ObjectId,
-      ref: 'Categoria',
-      required: true
-    },
-    'ativo': {
-      type: Boolean,
-      default: true,
-    },
-  });
+  },
+  'created': {
+    type: Date,
+    default: Date.now(),
+  },
+  'dbref': {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Categoria',
+    required: true
+  },
+  'ativo': {
+    type: Boolean,
+    default: true,
+  },
+});
 
-  return mongoose.model('Produto', schema);
-}
+module.exports = mongoose.model('Produto', schema)
