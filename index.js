b32ad19644a4cacd0eb76b3ad8563dbc3c934ad9
Brian_Izaki <bkizaki@outlook.com>
2020-11-14 16:15:02 -0300

    chore: alterado o banco

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./config/express")();
 
-require("./config/database")("mongodb://127.0.0.1/estoqueapi");
+require("./config/database")("mongodb+srv://admin:admin@cluster0-twcdj.mongodb.net/estoqueapi?retryWrites=true&w=majority");
 
 http.createServer(app).listen(app.get("port"), function () {
   console.log(`Express Server Escutando na porta ${app.get("port")}`);
