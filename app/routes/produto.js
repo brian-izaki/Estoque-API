75501f430ff69c25fc16f3ffa3b120109e2a7907
Leopitasurdogregorio <39489497+Leopitasurdogregorio@users.noreply.github.com>
2020-11-15 09:09:38 -0300

    Update produto.js

--- app/routes/produto.js
+++ app/routes/produto.js
@@ -1,5 +1,9 @@
-module.exports = function (app) {
+/* module.exports = function (app) {
   const controller = app.controllers.produto;
 
   app.post("/produto", controller.salvarProduto);
-}
+}*/
+
+/*
+Neste "produto.js" está rodando sem erro e funcionando nesta rota.
+*/
