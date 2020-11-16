4b754bdbca49ed3540ea6a4a7edf20df721a0d81
Samuel Silva <42380344+Samuel54mu3114@users.noreply.github.com>
2020-11-15 23:56:29 -0300

    Controller, Routes e Models
    Alterei o meu arquivo controller, models e routes

--- app/routes/estoque.js
+++ app/routes/estoque.js
@@ -1,7 +1,11 @@
-/*module.exports = (app) => {
+module.exports = function(app) {
     var controller = app.controllers.estoque;
-    app.post('/estoque', controller.salvarEstoque);
-}*/
+    app.post('/Estoque', controller.salvarEstoque);
+    // app.get('/Estoque', controller.listaEstoque);
+    // app.get('/Estoque/:id', controller.obtemEstoque);
+    // app.put('/Estoque', controller.alteraEstoque);
+    // app.delete('/Estoque/:id', controller.removeEstoque);
+}
 
 /* A execução desta rota que pode resultar num erro devido a "object undefined " dentro dos modules
  */
