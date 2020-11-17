283023c8dde5c746c8367e9f699ab58b9b103bb1
Samuel Silva <42380344+Samuel54mu3114@users.noreply.github.com>
2020-11-17 11:17:36 -0300

    controllers/estoque.js
    Fui adicionado as funções de alterar e remover, posteriormente sera adicionado o find e os testes parciais do que foi feito será realizado agora

--- app/controllers/estoque.js
+++ app/controllers/estoque.js
@@ -1,31 +1,58 @@
-module.exports = function (app){
+module.exports = function (app) {
 
-    const controller = {};
-  
-    const estoque = app.models.Estoque;
-  
-    controller.salvarEstoque = (req, res) => {
-      estoque
-        .create(req.body)
-        .then(
-          (sucesso)=> {
-            res.status(201).json(sucesso);
-          }, 
-          (error)=>{
-            res.status(500).json(error);
-          }
-      )
-    }
-  
-    controller.listarEstoque = (req, res) => {
-      estoque.find({}).exec().then(
-        (sucess) => {
-          res.status(200).json(sucess)
+  const controller = {};
+
+  const estoque = app.models.Estoque;
+
+  controller.salvarEstoque = (req, res) => {
+    estoque
+      .create(req.body)
+      .then(
+        (sucesso) => {
+          res.status(201).json(sucesso);
         },
         (error) => {
-          res.status(500).json(error)
+          res.status(500).json(error);
         }
       )
-    }
-    return controller;
   }
+
+  controller.listarEstoque = (req, res) => {
+    estoque.find({}).exec().then(
+      (sucess) => {
+        res.status(200).json(sucess)
+      },
+      (error) => {
+        res.status(500).json(error)
+      }
+    )
+  }
+
+  controller.alteraEstoque = (req, res) => {
+    var _id = req.body._id;
+    estoque.findByIdAndUpdate(_id, req.body).exec().then(
+      (estoque) => {
+        res.status(200).json(estoque);
+      },
+      (erro) => {
+        console.error(erro);
+        res.status(500).json(erro);
+      }
+    );
+  }
+
+  controller.removeEstoque = (req, res) => {
+    var _id = req.params._id;
+    estoque.remove({ "_id": _id }).exec().then(
+      (estoque) => {
+        res.status(204).end();
+      },
+      (erro) => {
+        console.error(erro);
+        res.status(500).json(erro);
+      }
+    );
+  }
+
+  return controller;
+}
