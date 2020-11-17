8162f98b70ad066e8949185cd8188a5ab8cf969a
Samuel Silva <42380344+Samuel54mu3114@users.noreply.github.com>
2020-11-17 11:06:04 -0300

    routes/estoque.js
    Foi atribuida outras rotas

--- app/routes/estoque.js
+++ app/routes/estoque.js
@@ -1,11 +1,8 @@
 module.exports = function(app) {
     var controller = app.controllers.estoque;
     app.post('/estoque', controller.salvarEstoque);
-    // app.get('/Estoque', controller.listaEstoque);
-    // app.get('/Estoque/:id', controller.obtemEstoque);
-    // app.put('/Estoque', controller.alteraEstoque);
-    // app.delete('/Estoque/:id', controller.removeEstoque);
+    app.get('/Estoque', controller.listaEstoque);
+    app.get('/Estoque/:id', controller.obtemEstoque);
+    app.put('/Estoque', controller.alteraEstoque);
+    app.delete('/Estoque/:id', controller.removeEstoque);
 }
-
-/* A execução desta rota que pode resultar num erro devido a "object undefined " dentro dos modules
- */
