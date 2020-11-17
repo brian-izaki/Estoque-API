9ac185063fb71ade3657242c443820891f2f2699
Samuel Silva <42380344+Samuel54mu3114@users.noreply.github.com>
2020-11-17 11:21:07 -0300

    Update estoque.js

--- app/routes/estoque.js
+++ app/routes/estoque.js
@@ -1,8 +1,8 @@
 module.exports = function(app) {
     var controller = app.controllers.estoque;
     app.post('/estoque', controller.salvarEstoque);
-    app.get('/Estoque', controller.listaEstoque);
-    app.get('/Estoque/:id', controller.obtemEstoque);
+    // app.get('/Estoque', controller.listaEstoque);
+    // app.get('/Estoque/:id', controller.obtemEstoque);
     app.put('/Estoque', controller.alteraEstoque);
     app.delete('/Estoque/:id', controller.removeEstoque);
 }
