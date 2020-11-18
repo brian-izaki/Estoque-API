0f15add638973d37415f744a7335e8386682f16a
Brian_Izaki <bkizaki@outlook.com>
2020-11-18 13:01:10 -0300

    feat: controller e routes da retirada

--- app/controllers/retirada.js
+++ app/controllers/retirada.js
@@ -28,5 +28,46 @@ module.exports = function (app){
     )
   }
 
+  controller.obtemRetirada = (req, res) => {
+    const _id = req.params.id;
+
+    retirada.findOne({_id}).exec().then(
+      (sucess) => {
+        res.status(200).json(sucess)
+      }, 
+      (error) => {
+        res.status(500).json(error)
+      }
+    )
+  }
+
+  controller.alteraRetirada = (req, res) => {
+    const _id = req.body._id;
+    retirada.findByIdAndUpdate(_id, req.body).exec().then(
+      (sucess) => {
+        if (!sucess){
+          res.status(404).json(sucess)
+        } else{
+          res.status(200).json(sucess)
+        }
+      },
+      (error) => {
+        res.status(500).json(error)
+      }
+    );
+  }
+
+  controller.removeRetirada = (req, res) => {
+    const _id = req.params.id;
+    retirada.remove({_id}).exec().then(
+      (sucess) => {
+        res.status(200).json(sucess)
+      },
+      (error) => {
+        res.status(500).json(error)
+      }
+    )
+  }
+
   return controller;
 }
--- app/routes/retirada.js
+++ app/routes/retirada.js
@@ -2,5 +2,8 @@ module.exports = function (app) {
   const controller = app.controllers.retirada;
 
   app.post('/retirada', controller.salvarRetirada);
-
+  app.get('/retirada', controller.listarRetiradas);
+  app.get('/retirada/:id', controller.obtemRetirada);
+  app.put('/retirada', controller.alteraRetirada);
+  app.delete('/retirada/:id', controller.removeRetirada);
 }
