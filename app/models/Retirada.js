5d6a9e7bb58db6bdc96d4a606188a9e3835f6683
Brian_Izaki <bkizaki@outlook.com>
2020-11-23 08:52:33 -0300

    refactor: mensagens de validação nos atributos

--- app/models/Retirada.js
+++ app/models/Retirada.js
@@ -1,28 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
-  "motivo": {
+  motivo: {
     type: String,
-    required: true,
+    required: [true, "É obrigatório o motivo da retirada"],
+    validate: {
+      validator: (value) => {
+        return value.length < 51;
+      },
+      message: "É permitido até 50 caracteres",
+    },
   },
-  "dbRefProduto": {
+  dbRefProduto: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Produto',
-    required: true
+    ref: "Produto",
+    required: [true, "É obrigatório o id do Produto"],
   },
-  "quantidade": {
+  quantidade: {
     type: Number,
-    required: true,
-    min: [0, 'Não é possível quantidade negativa']
+    required: [true, "É obrigatório a quantidade da retirada"],
+    min: [0, "Não é possível quantidade negativa"],
   },
-  "created": {
+  created: {
     type: Date,
-    default: Date.now()
-  }, 
-  "updated": {
+    default: Date.now(),
+  },
+  updated: {
     type: Date,
-    default: Date.now()
-  }
+    default: Date.now(),
+  },
 });
- 
-module.exports = app => mongoose.model('Retirada', schema);
+
+module.exports = (app) => mongoose.model("Retirada", schema);
