36b836f904cfcccf2ae3c82a8d3b448de143bf90
Brian_Izaki <bkizaki@outlook.com>
2020-11-29 18:13:12 -0300

    feat: campo updated para Produto

--- app/models/Produto.js
+++ app/models/Produto.js
@@ -1,31 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 module.exports = (app) => {
-    const schema = mongoose.Schema({
-        'descricao': {
-            type: String,
-            required: true,
-        },
-        'valor': {
-            type: Number,
-            required: true,
-            min: [0, 'Valor deve ser sempre positivo']
+  const schema = mongoose.Schema({
+    descricao: {
+      type: String,
+      required: [true, "É obrigatória a descrição do produto "],
+    },
+    valor: {
+      type: Number,
+      required: true,
+      min: [0, "Valor deve ser sempre positivo"],
+    },
+    dbref: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Categoria",
+      required: [true, "É obrigatório o id de referência da Categoria"],
+    },
+    ativo: {
+      type: Boolean,
+      default: true,
+    },
+    created: {
+      type: Date,
+      default: Date.now(),
+    },
+    updated: {
+      type: Date,
+      default: Date.now(),
+    },
+  });
 
-        },
-        'created': {
-            type: Date,
-            default: Date.now(),
-        },
-        'dbref': {
-            type: mongoose.Schema.ObjectId,
-            ref: 'Categoria',
-            required: true
-        },
-        'ativo': {
-            type: Boolean,
-            default: true,
-        },
-    });
-
-    return mongoose.model('Produto', schema);
-}
+  return mongoose.model("Produto", schema);
+};
